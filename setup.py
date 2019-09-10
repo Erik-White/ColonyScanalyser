04ead5d734114cbd58557b7f7fac53a7b4a3827d
Erik White <26148654+Erik-White@users.noreply.github.com>
2019-09-10 17:17:40 +0300

    Restructure as a package

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    python_requires = ">3.7"
+    name = "scanlag",
+    version = "0.1",
+    description = "Runs ScanLag analysis on image files",
+    url = "https://github.com/Erik-White/ScanLag/",
+    author = "",
+    author_email = "",
+    license = "GPL-3.0",
+    packages =  find_packages(),
+    zip_safe = False
+    install_requires = [
+        "numpy",
+        "matplotlib",
+        "scikit-image >= 0.16"
+    ],
+    extras_require = {
+        #"dev": ["check-manifest"],
+        "test": [
+            "mock",
+            "pytest",
+            "pytest-cov"
+        ],
+    }
+)
