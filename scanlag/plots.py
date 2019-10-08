0e27b7efd1c8bf66fe6d454989002c021cada519
Erik White <26148654+Erik-White@users.noreply.github.com>
2019-10-08 15:26:18 +0300

    Combined plot_appearance_frequency and plot_appearance_frequency_all to one method

--- scanlag/plots.py
+++ scanlag/plots.py
@@ -104,21 +104,22 @@ def colony_growth_curve(ax, plate_item, time_points_dict, scatter_color, line_co
     ax.set_ylabel("Colony area (pixels)")
 
 
-
-def plot_appearance_frequency_all(plate_list, plate_lattice, time_points_elapsed, save_path):
+def plot_appearance_frequency(plates_dict, time_points_elapsed, save_path, bar = False):
     """
     Compare time of appearance frequency on all plates of the plate lattice
     """
     fig, ax = plt.subplots()
     colormap = cm.get_cmap("plasma")
+    
+    for plate_item in plates_dict.items():
+        if len(plates_dict.keys()) > 1:
+            # Get a color from the colourmap
+            cm_plate = colormap(0.2 + (0.65 - 0.2) * (plate_item[0] / len(plates_dict.keys())))
+        else:
+            cm_plate = "Purple"
 
-    # Plot areas for each colony
-    for plate_item in plate_list.items():
-        # Get a color from the colourmap
-        cm_plate = colormap(0.2 + (0.65 - 0.2) * (plate_item[0] / len(plate_list.keys())))
-
-        # Add the growth curve plot for this plate
-        time_of_appearance_frequency(ax, plate_item, time_points_elapsed, cm_plate)
+        # Plot frequency for each time point
+        time_of_appearance_frequency(ax, plate_item, time_points_elapsed, cm_plate, bar = bar)
 
     lgd = ax.legend(loc = 'center right', fontsize = 8, bbox_to_anchor = (1.25, 0.5))
     save_params = {"format": "png",
@@ -128,53 +129,46 @@ def plot_appearance_frequency_all(plate_list, plate_lattice, time_points_elapsed
     
     plt.ylim(ymin = 0)
     plt.title("Time of appearance")
-    fig.savefig(str(save_path.joinpath("time_of_appearance_comparison.png")), **save_params)
+    if bar:
+        save_name = "time_of_appearance_bar.png"
+    else:
+        save_name = "time_of_appearance.png"
+    fig.savefig(str(save_path.joinpath(save_name)), **save_params)
 
     plt.close()
 
 
-def plot_appearance_frequency(plate_item, time_points_elapsed, save_path):
+def time_of_appearance_frequency(ax, plate_item, time_points_dict, line_color, bar = False):
     """
-    Time of appearance frequency for a single plate
-    """
-    fig, ax = plt.subplots()
-
-    # Plot areas for each colony
-    time_of_appearance_frequency(ax, plate_item, time_points_elapsed, "Purple")
-
-    lgd = ax.legend(loc = 'center right', fontsize = 8, bbox_to_anchor = (1.25, 0.5))
-    save_params = {"format": "png",
-          "bbox_extra_artists": (lgd,),
-          "bbox_inches": "tight"
-          }
-    
-    plt.ylim(ymin = 0)
-    plt.title("Time of appearance")
-    fig.savefig(str(save_path.joinpath("time_of_appearance.png")), **save_params)
-
-    plt.close()
-
-
-def time_of_appearance_frequency(ax, plate_item, time_points_dict, line_color):
-    """
-    Add a time of appearance frequency plot to an axis
+    Add a time of appearance frequency bar or line plot to an axis
     """
     plate_id, plate = plate_item
 
     # Map counts to a dictionary of all timepoints
-    time_points_dict = dict.fromkeys(time_points_dict, 0)
+    time_points_dict = dict()
     for colony in plate.values():
-        time_points_dict[colony.timepoint_first.elapsed_minutes] += 1
+        key = colony.timepoint_first.elapsed_minutes
+        if key not in time_points_dict:
+            time_points_dict[key] = 0
+        time_points_dict[key] += 1
 
     # Normalise counts to frequency
     time_points_dict = {key: value / len(time_points_dict.keys()) for key, value in time_points_dict.items()}
 
     # Use zip to return a sorted list of tuples (key, value) from the dictionary
-    ax.plot(*zip(*sorted(time_points_dict.items())),
-        color = line_color,
-        label = f"Plate {plate_id}",
-        alpha = 0.9,
-        )
+    if not bar:
+        ax.plot(*zip(*sorted(time_points_dict.items())),
+            color = line_color,
+            label = f"Plate {plate_id}",
+            alpha = 0.9
+            )
+    else:
+        ax.bar(*zip(*sorted(time_points_dict.items())),
+            width = 10,
+            color = line_color,
+            label = f"Plate {plate_id}",
+            alpha = 0.6
+            )
 
     # Format x-axis labels as integer hours
     ax.set_xticklabels(axis_minutes_to_hours(ax.get_xticks()))
