6f7f7204a8f8d818f328eb0c9644dd92ee5b8f15
Erik White <26148654+Erik-White@users.noreply.github.com>
2019-10-13 18:21:07 +0300

    Fix command line entry point

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", 'r') as f:
 setup(
     python_requires = ">3.7",
     name = "colonyscanalyser",
-    version = "0.1.0",
+    version = "0.1.2",
     description = "An image analysis tool for measuring microorganism colony growth",
     long_description = long_description,
     url = "https://github.com/Erik-White/ColonyScanalyser/",
@@ -33,7 +33,7 @@ setup(
     },
     entry_points={
         'console_scripts': [
-            'scanalyser = colonyscanalyser:main',
+            'scanalyser = colonyscanalyser.colonyscanalyser:main',
         ],
     },
 )
