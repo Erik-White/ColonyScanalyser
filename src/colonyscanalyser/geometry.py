f72921abde497aff8cde165ce4c31042bd8d4d2a
Erik White <26148654+Erik-White@users.noreply.github.com>
2020-01-18 17:23:25 +0200

    Added geometry module with Circle class

---
+++ src/colonyscanalyser/geometry.py
@@ -0,0 +1,32 @@
+from math import pi
+
+
+class Circle:
+    """
+    An object to generate the properties of a circle
+    """
+    def __init__(self, diameter: float):
+        self.diameter = diameter
+
+    @property
+    def area(self):
+        return pi * self.radius * self.radius
+
+    @property
+    def circumference(self):
+        return pi * self.diameter
+
+    @property
+    def diameter(self):
+        return self.__diameter
+
+    @diameter.setter
+    def diameter(self, val: float):
+        if val <= 0:
+            raise ValueError("The diameter must be a number greater than zero")
+
+        self.__diameter = val
+
+    @property
+    def radius(self):
+        return self.diameter / 2
---
+++ tests/unit/test_geometry.py
@@ -0,0 +1,41 @@
+import pytest
+
+from math import pi
+from colonyscanalyser.geometry import (
+    Circle
+    )
+
+
+class TestCircle():
+    @pytest.fixture(params = [0.1, 1, 1.5, 1000])
+    def diameter(self, request):
+        yield request.param
+
+    @pytest.fixture(params = [-1, 0, "1", "one"])
+    def diameter_invalid(self, request):
+        yield request.param
+
+    def test_area(self, diameter):
+        circle = Circle(diameter)
+        radius = diameter / 2
+
+        assert circle.area == pi * radius * radius
+
+    def test_circumference(self, diameter):
+        circle = Circle(diameter)
+
+        assert circle.circumference == pi * diameter
+
+    def test_diameter(self, diameter):
+        circle = Circle(diameter)
+
+        assert circle.diameter == diameter
+
+    def test_radius(self, diameter):
+        circle = Circle(diameter)
+
+        assert circle.radius == diameter / 2
+
+    def test_invalid(self, diameter_invalid):
+        with pytest.raises((TypeError, ValueError)):
+            Circle(diameter_invalid)
