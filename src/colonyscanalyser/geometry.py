b577f817f10ed6d5046ec35aa3d6f8b1dafca570
Erik White <26148654+Erik-White@users.noreply.github.com>
2020-01-20 13:56:47 +0200

    Add Shape base class to provide the fundamental properties of a surface

--- src/colonyscanalyser/geometry.py
+++ src/colonyscanalyser/geometry.py
@@ -1,7 +1,64 @@
 from math import pi
 
 
-class Circle:
+class Shape:
+    """
+    An abstract class to provide the fundamental properties of a surface
+    """
+    @property
+    def area(self):
+        raise NotImplementedError("This property must be implemented in a derived class")
+
+    @property
+    def center(self):
+        try:
+            return self.__center
+        except AttributeError:
+            return None
+
+    @center.setter
+    def center(self, val: tuple):
+        self.__center = val
+
+    @property
+    def depth(self):
+        try:
+            return self.__depth
+        except AttributeError:
+            return 0
+
+    @depth.setter
+    def depth(self, val: float):
+        self.__depth = val
+
+    @property
+    def height(self):
+        try:
+            return self.__height
+        except AttributeError:
+            return 0
+
+    @height.setter
+    def height(self, val: float):
+        self.__height = val
+
+    @property
+    def perimeter(self):
+        raise NotImplementedError("This property must be implemented in a derived class")
+
+    @property
+    def width(self):
+        try:
+            return self.__width
+        except AttributeError:
+            return 0
+
+    @width.setter
+    def width(self, val: float):
+        self.__width = val
+
+
+class Circle(Shape):
     """
     An object to generate the properties of a circle
     """
@@ -14,7 +71,7 @@ class Circle:
 
     @property
     def circumference(self):
-        return pi * self.diameter
+        return self.perimeter
 
     @property
     def diameter(self):
@@ -28,5 +85,17 @@ class Circle:
         self.__diameter = val
 
     @property
+    def height(self):
+        return self.diameter
+
+    @property
+    def perimeter(self):
+        return pi * self.diameter
+
+    @property
     def radius(self):
         return self.diameter / 2
+
+    @property
+    def width(self):
+        return self.diameter
--- tests/unit/test_geometry.py
+++ tests/unit/test_geometry.py
@@ -2,8 +2,52 @@ import pytest
 
 from math import pi
 from colonyscanalyser.geometry import (
+    Shape,
     Circle
     )
+
+
+class TestShape:
+    @pytest.fixture
+    def shape(self, request):
+        yield Shape()
+
+    @pytest.fixture(params = [0, 1, 1.1, -1])
+    def distance(self, request):
+        yield request.param
+
+    def test_area(self, shape):
+        with pytest.raises(NotImplementedError):
+            shape.area
+
+    @pytest.mark.parametrize("center", [(0, 0), (1, 1), (0, -1), (0, 0, 0), (1, 1, 1)])
+    def test_center(self, shape, center):
+        assert shape.center is None
+
+        shape.center = center
+        assert shape.center == center
+
+    def test_depth(self, shape, distance):
+        assert shape.depth == 0
+
+        shape.depth = distance
+        assert shape.depth == distance
+
+    def test_height(self, shape, distance):
+        assert shape.height == 0
+
+        shape.height = distance
+        assert shape.height == distance
+
+    def test_perimeter(self, shape):
+        with pytest.raises(NotImplementedError):
+            shape.perimeter
+
+    def test_width(self, shape, distance):
+        assert shape.width == 0
+
+        shape.width = distance
+        assert shape.width == distance
 
 
 class TestCircle():
@@ -31,11 +75,26 @@ class TestCircle():
 
         assert circle.diameter == diameter
 
+    def test_height(self, diameter):
+        circle = Circle(diameter)
+
+        assert circle.height == diameter
+
+    def test_perimeter(self, diameter):
+        circle = Circle(diameter)
+
+        assert circle.perimeter == pi * diameter
+
     def test_radius(self, diameter):
         circle = Circle(diameter)
 
         assert circle.radius == diameter / 2
 
-    def test_invalid(self, diameter_invalid):
+    def test_width(self, diameter):
+        circle = Circle(diameter)
+
+        assert circle.width == diameter
+
+    def test_diameter_invalid(self, diameter_invalid):
         with pytest.raises((TypeError, ValueError)):
             Circle(diameter_invalid)
