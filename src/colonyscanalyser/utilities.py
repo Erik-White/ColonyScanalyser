6bb39aafec30dc75f165305455bb3f719a859915
Erik White <26148654+Erik-White@users.noreply.github.com>
2020-02-26 14:13:19 +0200

    Add type hints and missing docstrings to utilities module

--- src/colonyscanalyser/utilities.py
+++ src/colonyscanalyser/utilities.py
@@ -1,11 +1,21 @@
-﻿def round_tuple_floats(tuple_item, precision = 2):
+﻿from typing import Tuple, List, Dict
+
+
+def round_tuple_floats(tuple_item: Tuple[float], precision: int = 2) -> Tuple[float]:
+    """
+    Round float values within a tuple to the required precision
+
+    :param tuple_item: a tuple of floats
+    :param precision: the rounding value for the floats in tuple_items
+    :returns: a tuple of rounded values
+    """
     if not isinstance(tuple_item, tuple):
         raise ValueError(f"The object must be of type 'tuple', not type '{type(tuple_item)}'")
 
     return tuple(map(lambda x: isinstance(x, float) and round(x, precision) or x, tuple_item))
 
 
-def progress_bar(bar_progress, bar_length = 30, message = ""):
+def progress_bar(bar_progress: float, bar_length: float = 30, message: str = ""):
     """
     Output a simple progress bar to the console
 
@@ -27,7 +37,7 @@ def progress_bar(bar_progress, bar_length = 30, message = ""):
     stdout.flush()
 
 
-def average_dicts_values_by_key(dicts):
+def average_dicts_values_by_key(dicts: List[Dict]) -> Dict:
     """
     Mean average values across multiple dicts with the same key
 
@@ -45,7 +55,7 @@ def average_dicts_values_by_key(dicts):
     return {x: float(sums[x]) / counters[x] for x in sums.keys()}
 
 
-def average_median_dicts_values_by_key(dicts):
+def average_median_dicts_values_by_key(dicts: List[Dict]) -> Dict:
     """
     Median average values across multiple dicts with the same key
 
